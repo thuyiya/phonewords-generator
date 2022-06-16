e6a6dfa09700c33d187e6811d24a5d15eec9c06c
thuyiya <thusitha.4t@gmail.com>
2022-06-16 14:03:45 +0530

    export build to prod

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
+
+// This uses phases as outlined here: https://nextjs.org/docs/#custom-configuration
+module.exports = (phase) => {
+  // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER
+  // when `next build` or `npm run build` is used
+  const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+  // when `next build` or `npm run build` is used
+  const isStaging =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1'
+
+  const env = {
+    APP_ENVIRONMENT: (() => {
+      if (isDev) return 'development'
+      if (isProd) return 'production'
+      if (isStaging) return 'staging'
+
+      return 'local'
+    })(),
+  }
+
+  console.log("env ", env)
+
+  return {
+    env,
+    reactStrictMode: true,
+    optimizeFonts: true
+  }
+}
