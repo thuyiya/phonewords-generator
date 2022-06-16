7772d0e25428aa8e84715df67fb9ef8447d4a1a9
thuyiya <thusitha.4t@gmail.com>
2022-06-16 11:02:21 +0530

    remove unnecessary

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome 
-        </h1>
-      </main>
+    <div>
+      <h1>Welcome</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
