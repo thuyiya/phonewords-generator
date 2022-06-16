4d92309d5df639c9aef1d705789a53d6309b3d9a
thuyiya <thusitha.4t@gmail.com>
2022-06-16 11:05:19 +0530

    separate home

---
+++ frontend/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export { Home };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,2 @@
-import type { NextPage } from "next";
-import Head from "next/head";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
-  );
-};
-
-export default Home;
+export { Home as default } from 'components/Home/Home'
